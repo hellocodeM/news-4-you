40676b9a8505fa25478fde6864e588da40b45595
HelloCodeMing <huanmingwong@163.com>
2014-12-25 16:05:05 +0800

    fuck sbt

---
+++ scala/build.sbt
@@ -0,0 +1,5 @@
+name := "news4you"
+
+version := "0.0.1"
+
+libraryDependencies += "org.mongodb" %% "casbah" % "2.7.4"
---
+++ test/test-site-watcher.js
@@ -0,0 +1,4 @@
+var watcher = require('site-watcher');
+var sites = ["http://wanghuanminbg.com", "http://ruanyifeng.com", "http://baidu.com"];
+
+watcher.watch(sites);
