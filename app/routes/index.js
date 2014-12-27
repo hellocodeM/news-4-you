6ed0c407ec675a8a4730345be4361ccbd46916da
HelloCodeMing <huanmingwong@163.com>
2014-12-27 16:17:08 +0800

    add property configuration

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var db = require('mongous').Mongous;
+var yaml = require('js-yaml');
+var fs = require('fs');
+var property = yaml.safeLoad(fs.readFileSync('../config/property', 'utf-8'));
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    db('news.$cmd').auth(property.user, property.passwd, function(r) {
+        console.log(r);
+        db('news.resource')
+    });
+	var varibles = {
+		articles : articles,
+		title: 'news 4 you'
+	};
+	res.render('index', varibles);
 });
 
 module.exports = router;
