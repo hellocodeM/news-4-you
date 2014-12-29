51e6c80f4ad42781f929c834bcf21fbfcde1efe6
HelloCodeMing <huanmingwong@163.com>
2014-12-29 17:16:07 +0800

    add articleid for recommend

--- bin/addID.js
+++ bin/addID.js
@@ -1,10 +1,21 @@
 // mongo script
 
-var index = 1;
-var cursor = db.resource.article.find();
+//var index = 1;
+//var cursor = db.resource.article.find();
+//while (cursor.hasNext()) {
+//    var doc = cursor.next();
+//    doc.articleid = index++;
+//    db.resource.article.save(doc);
+//}
+//
+
+var cursor = db.recommend.similar.find();
 while (cursor.hasNext()) {
     var doc = cursor.next();
-    doc.articleid = index++;
-    db.resource.article.save(doc);
+    var id = doc._id;
+    doc.articleid = db.resource.article.findOne({_id: id}).articleid;
+    doc.similar = doc.similar.map(function(_id) {
+        return db.resource.article.findOne({_id: _id}).articleid;
+    });
+    db.recommend.similar.save(doc);
 }
-
