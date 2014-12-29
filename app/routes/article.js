f565355e4f96f7d034a4335db9949e10e94ad564
HelloCodeMing <huanmingwong@163.com>
2014-12-29 18:56:46 +0800

    bug fix: not union but intersection

--- app/routes/article.js
+++ app/routes/article.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var db = require('mongous').Mongous;
 var yaml = require('js-yaml');
 var fs = require('fs');
-var ObjectID = require('../../lib/objectid').ObjectID;
 
 var property = yaml.safeLoad(fs.readFileSync('../config/property.yaml', 'utf-8'));
 
@@ -17,11 +16,29 @@ router.get('/:articleid', function(req, res) {
             if (!doc)
                 doc = { title: 'oops', content: 'we can not find the article you want', keywords: [] };
             else {
+                // find the keywords
                 doc.keywords = doc.keywords.map(function(keyword) {
                     return keyword[0];
                 });
+                // find the articleid of similar articles and find the meta data or these articles
+                db('news.recommend.similar').find({articleid: doc.articleid}, function(reply2) {
+                    var similarArticles = [];
+                    reply2.documents[0].similar.forEach(function(articleid) {
+                        db('news.resource.article').find({articleid: articleid}, 1, function(reply3) {
+                            var doc = reply3.documents[0];
+                            similarArticles.push({title: doc.title, link: '/article/' + articleid});
+                        });
+                    });
+                    setTimeout(function() {
+                        res.render('article', {
+                            title: doc.title,
+                            content: doc.content,
+                            keywords: doc.keywords,
+                            recommends: similarArticles
+                        });
+                    }, 1000);
+                });
             }
-            res.render('article', doc);
         });
     });
 });
--- scala/src/main/scala/com/wanghuanming/news4you/similarity.scala
+++ scala/src/main/scala/com/wanghuanming/news4you/similarity.scala
@@ -5,43 +5,82 @@ import com.mongodb.casbah.Imports._
 /**
   * description: get similar articles of each article, store there id in mongodb://recommend/similar
   */
+
+//case class Article(
+//  id = new ObjectId(),
+//  title: String,
+//  content: String,
+//  keywords: List[List] = List[List](),
+//  articleid: Int
+//)
+//
+//case class Recommend(
+//  id: ObjectId = new ObjectId(),
+//  articleid: Int,
+//  similar = List[Int]
+//)
+//
+//object ArticleMap {
+//  def toBson(article: Article) = {
+//    MongoDBObject(
+//      "_id" -> article.id,
+//      "title" -> article.title,
+//      "content" -> article.content,
+//      "keywords" -> article.keywords,
+//      "articleid" -> article.articleid
+//    )
+//  }
+//
+//  def fromBson(o: MongoDBObject) = {
+//    Article(
+//      _id = o.as[ObjectId]("_id"),
+//      
+//    )
+//  }
+//}
 object similariry {
   def main(args: Array[String]) = {
     val news = MongoClient()("news")
     news.authenticate("ming", "00")
     val articles = news("resource.article")
     val recommend = news("recommend.similar")
-    val matrix = articles.find(MongoDBObject(), MongoDBObject("_id" -> 1, "keywords" -> 1)).toList
+    val matrix = articles.find().toList
     
     matrix.foreach { article =>
-      // get keywords of each article
-      val id = article.getAsOrElse("_id", new ObjectId())
+      // get articleid and keywords of each article
+      val id = article.as[Double]("articleid").toInt
       val keywords = article.getAsOrElse("keywords", List[BasicDBList]())
-      // get similar articles
-      val res = matrix.map { arti => 
-        arti.getAsOrElse("_id", new ObjectId()) -> 
+      // get similarest 5 articles
+      val similarArticles = matrix.map { arti => 
+        // similariry between article and arti
+        arti.as[Double]("articleid").toInt -> 
           similar(keywords, arti.getAsOrElse("keywords", List[BasicDBList]())) 
         }.sortBy(_._2).map(_._1).reverse.take(5)
-      val obj = MongoDBObject("_id" -> id, "similar" -> res)
-      recommend.insert(obj)
+      val obj = MongoDBObject("articleid" -> id, "similar" -> similarArticles)
+      recommend.save(obj)
     }
   }
 
+  /**
+    * @desc: similarity between two articles
+    * @return: Double value between 0 and 1, if bigger, then more similar
+    */
 
   def similar(A: List[BasicDBList], B: List[BasicDBList]): Double = {
     val mapA = A.map(list => list(0).toString -> list(1).toString.toDouble).toMap
     val mapB = B.map(list => list(0).toString -> list(1).toString.toDouble).toMap
-    val share = mapA.map(_._1).toList.union(mapB.map(_._1).toList).distinct
-    if (share.length == 0)
+    // find the keywords they share with
+    var intersection = A.map(_(0).toString).intersect(B.map(_(0).toString))
+    if (intersection.length == 0)
       return 0.0
-    val sum = share.map { word =>
+    val sum = intersection.map { word =>
       mapA.getOrElse(word, 0.0) * mapB.getOrElse(word, 0.0)
     }.reduce(_ + _)
-    val sumA = share.map { word =>
+    val sumA = intersection.map { word =>
       val self = mapA.getOrElse(word, 0.0) 
       self * self
     }.reduce(_ + _)
-    val sumB = share.map { word => 
+    val sumB = intersection.map { word => 
       val self = mapB.getOrElse(word, 0.0)
       self * self
     }.reduce(_ + _)
