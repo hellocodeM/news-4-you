3d60221c7710369dbd063035141b711318b3c21f
HelloCodeMing <huanmingwong@163.com>
2014-12-29 19:27:19 +0800

    finish recommend

--- app/routes/article.js
+++ app/routes/article.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var db = require('mongous').Mongous;
 var yaml = require('js-yaml');
 var fs = require('fs');
+var async = require('async');
 
 var property = yaml.safeLoad(fs.readFileSync('../config/property.yaml', 'utf-8'));
 
@@ -22,21 +23,23 @@ router.get('/:articleid', function(req, res) {
                 });
                 // find the articleid of similar articles and find the meta data or these articles
                 db('news.recommend.similar').find({articleid: doc.articleid}, function(reply2) {
-                    var similarArticles = [];
-                    reply2.documents[0].similar.forEach(function(articleid) {
-                        db('news.resource.article').find({articleid: articleid}, 1, function(reply3) {
+                    async.map(reply2.documents[0].similar, function(arti, cb) {
+                        db('news.resource.article').find({articleid: arti}, 1, function(reply3) {
                             var doc = reply3.documents[0];
-                            similarArticles.push({title: doc.title, link: '/article/' + articleid});
+                            cb(null, {title: doc.title, link: '/article/' + doc.articleid});
                         });
+                    }, function(err, result) {
+                        if (err)
+                            console.log(err);
+                        else {
+                            res.render('article', {
+                                title: doc.title,
+                                content: doc.content,
+                                keywords: doc.keywords,
+                                recommends: result 
+                            });
+                        }
                     });
-                    setTimeout(function() {
-                        res.render('article', {
-                            title: doc.title,
-                            content: doc.content,
-                            keywords: doc.keywords,
-                            recommends: similarArticles
-                        });
-                    }, 1000);
                 });
             }
         });
